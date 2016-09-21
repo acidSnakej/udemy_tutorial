4fd04535078811f5cf63b0d310b0df82d16a5784
Jorge Rivera <jrivera600@itfip.edu.co>
2016-09-20 22:19:58 -0500

    Limit of flappy world
    Learning basic of physics

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var backgroundGame;
 var button;
 var flappy;
 var cursors;
+var person;
 var mainState = {
     
     preload: function () {
@@ -11,6 +12,7 @@ var mainState = {
         "use strict";
         game.load.image('background', 'img/bg.jpeg');
         game.load.spritesheet('birds', 'img/pajaros.png', 43, 30); // depends of the dimension of image, for example this image is 129x30,                                                                       therefor 129/3 = 43, then the first bird appears. 30 is the value of Y
+        game.load.spritesheet('persons', 'img/persona.png', 64, 64);
 
     },
 
@@ -24,6 +26,10 @@ var mainState = {
                                                           // second argument array order position of animation
                                                          // Thir argument speed of animation
         cursors = game.input.keyboard.createCursorKeys(); // create all cursors
+        game.physics.startSystem(Phaser.Physics.ARCADE); // All physics 
+        game.physics.arcade.enable(flappy);
+        flappy.body.collideWorldBounds = true; // limit of windows or world flappy cant advance.
+        
     },
 
     update: function () {
@@ -32,19 +38,19 @@ var mainState = {
         flappy.animations.play('fly');
         if(cursors.right.isDown)
             {
-                flappy.position.x += 1;
+                flappy.position.x += 5;
             }
         if(cursors.down.isDown)
             {
-                flappy.position.y += 1;
+                flappy.position.y += 5;
             }
         if(cursors.left.isDown)
             {
-                flappy.position.x -= 1;
+                flappy.position.x -= 5;
             }
         if(cursors.up.isDown)
             {
-                flappy.position.y -= 1;
+                flappy.position.y -= 5;
             }
         backgroundGame.tilePosition.x -= 10;
         
