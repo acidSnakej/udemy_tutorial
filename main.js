641b76dd21dc5dc672ad75abc5254d8e310baf92
Jorge Rivera <jrivera600@itfip.edu.co>
2016-09-20 22:46:04 -0500

    Learning multiple animations on one sprite

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ var mainState = {
         // Show all recurses
         "use strict";
         backgroundGame = game.add.tileSprite(0, 0, 370, 550, 'background');
+        /*
         flappy = game.add.sprite(100, 100, 'birds');
         flappy.frame = 1; // with this take the 3 images like a array, the second bird is the position 1 by default
         flappy.animations.add('fly', [0, 1, 2], 10, true); // first argument name of animation, 
@@ -29,30 +30,52 @@ var mainState = {
         game.physics.startSystem(Phaser.Physics.ARCADE); // All physics 
         game.physics.arcade.enable(flappy);
         flappy.body.collideWorldBounds = true; // limit of windows or world flappy cant advance.
+        */
+        person = game.add.sprite(game.width/2, game.height/2, 'persons');
+        person.anchor.setTo(0.5);
+        person.animations.add('right', [27, 28, 29, 30, 31, 32, 33, 34, 35], 10, true); // depends the position the sprite to animate
+        person.animations.add('up', [0, 1, 2, 3, 4, 5, 6, 7, 8], 10, true);
+        person.animations.add('down', [18, 19, 20, 21, 23, 24, 25, 26], 10, true);
+        person.animations.add('left', [9, 10, 11, 12, 13, 14, 15, 16, 17], 10, true);
+        game.physics.arcade.enable(person);
+        person.body.collideWorldBounds = true;
+        cursors = game.input.keyboard.createCursorKeys();
+        
+        
         
     },
 
     update: function () {
         "use strict";
         // Animate the game
-        flappy.animations.play('fly');
+        //flappy.animations.play('fly');
         if(cursors.right.isDown)
             {
-                flappy.position.x += 5;
+                person.animations.play('right');
+                person.position.x += 2;
+                // flappy.position.x += 5;
             }
         if(cursors.down.isDown)
             {
-                flappy.position.y += 5;
+                person.animations.play('down');
+                person.position.y += 2;
+                // flappy.position.y += 5;
             }
         if(cursors.left.isDown)
             {
-                flappy.position.x -= 5;
+                person.animations.play('left');
+                person.position.x -= 2;
+                // flappy.position.x -= 5;
+                
             }
         if(cursors.up.isDown)
             {
-                flappy.position.y -= 5;
+                person.animations.play('up');
+                person.position.y -= 2;
+                // flappy.position.y -= 5;
             }
-        backgroundGame.tilePosition.x -= 10;
+        backgroundGame.tilePosition.x -= 2;
+        
         
     }
 
