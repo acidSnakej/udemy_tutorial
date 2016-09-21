5f7a78748a404e962b2ad63e52c33810a55045d8
Jorge Rivera <jrivera600@itfip.edu.co>
2016-09-20 23:42:36 -0500

    Learning how shoot in space invader

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var game = new Phaser.Game(370, 550, Phaser.AUTO, 'block_game');
 var backgroundGame;
 var ship;
 var cursors;
+var bullets;
+var bulletTime = 0;
+var shootButton;
+
 var mainState = {
     
     preload: function () {
@@ -10,6 +14,7 @@ var mainState = {
         "use strict";
         game.load.image('background', 'img/space.png');
         game.load.image('ship', 'img/nave.png');
+        game.load.image('laser', 'img/laser.png');
 
     },
 
@@ -20,6 +25,15 @@ var mainState = {
         ship = game.add.sprite(game.width/2, 500, 'ship');
         ship.anchor.setTo(0.5);
         cursors = game.input.keyboard.createCursorKeys();
+        shootButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        bullets = game.add.group();
+        bullets.enableBody = true; // Prepare to collision
+        bullets.physicsBodyType = Phaser.Physics.ARCADE; // Prepare the body to bullets
+        bullets.createMultiple(20, 'laser'); // How many bullets
+        bullets.setAll('anchor.x', 0.5); // From where is shooting
+        bullets.setAll('anchor.y', 1); // From where is shooting
+        bullets.setAll('outOfBoundsKill', true); // If the Bullet reach the limit of the world dies
+        bullets.setAll('checkWorldBounds', true); // Verify where is the bullet
         
         
     },
@@ -33,6 +47,19 @@ var mainState = {
         else if(cursors.left.isDown)
             {
                 ship.position.x -= 3;
+            }
+        if(shootButton.isDown)
+            {
+                if(game.time.now > bulletTime)
+                {
+                    var bullet = bullets.getFirstExists(false);
+                }
+                if(bullet)
+                {
+                    bullet.reset(ship.x, ship.y);
+                    bullet.body.velocity.y = -300;
+                    bulletTime = game.time.now + 800;
+                }
             }
         
     }
